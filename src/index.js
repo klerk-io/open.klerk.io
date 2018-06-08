6ce26da7623a857d333aa4a7e5b0f7bdc32bbcb0
Philipp Bauer <hello@philippbauer.org>
2018-06-08 15:03:47 -0500

    Remove @google-cloud/trace-agent

--- src/index.js
+++ src/index.js
@@ -5,14 +5,6 @@
  *
  * @author  Philipp Bauer <pbauer@klerk.io>
  */
-require("dotenv").config();
-
-if (process.env.APP_PLATFORM === "gcloud") {
-  console.log("Start Google Cloud Trace Agent");
-  require('@google-cloud/trace-agent').start({
-    plugins: { 'modofun': 'modofun-trace-agent-plugin' }
-  });
-}
 
 // Export modofun services as function handler
 exports.lockers = require("./app/services/LockerService");
