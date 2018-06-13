a86848848bee3125d16d0ba86f30872468ef3172
Philipp Bauer <hello@philippbauer.org>
2018-06-12 19:14:42 -0500

    Added middleware to modofun

--- src/app/services/LockerService.js
+++ src/app/services/LockerService.js
@@ -4,20 +4,33 @@ const config = require("../config");
 const modofun = require("modofun");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const accesscontrol = require("../middleware/AccessControl");
+const options = require("../middleware/OptionsResponse");
 
 const LockerController = require("../controllers/LockerController").default;
 let lockerController = new LockerController(config);
 
-module.exports = modofun({
-  "fetch": [modofun.arity(1), function(id) {
-    return lockerController.get(id);
-  }],
-  "yield": [modofun.arity(0), function() {
-    return lockerController.store(this.body);
-  }]
-}, {
-  middleware: [
-    morgan('tiny'),
-    helmet()
-  ]
-});
+module.exports = modofun(
+  {
+    fetch: [
+      modofun.arity(1),
+      function(id) {
+        return lockerController.get(id);
+      }
+    ],
+    yield: [
+      modofun.arity(0),
+      function() {
+        return lockerController.store(this.body);
+      }
+    ]
+  },
+  {
+    middleware: [
+      morgan("tiny"),
+      accesscontrol(config.headers.access),
+      options(),
+      helmet()
+    ]
+  }
+);
