32168f9350d6a7a8ce1162b785743532a51b06af
Philipp Bauer <hello@philippbauer.org>
2018-06-12 19:11:45 -0500

    Restructured and added headers

--- src/app/config.js
+++ src/app/config.js
@@ -7,12 +7,19 @@ module.exports = {
     }
   },
   headers: {
-    sts: process.env.HEADERS_STRICT_TRANSPORT_SECURITY || "max-age=31536000; includeSubDomains",
-    csp: process.env.HEADERS_CONTENT_SECURITY_POLICY || "default https:",
-    xfo: process.env.HEADERS_X_FRAME_OPTIONS || "SAMEORIGIN",
-    xss: process.env.HEADERS_X_XSS_PROTECTION || "1; mode=block",
-    xco: process.env.HEADERS_X_CONTENT_TYPE_OPTIONS || "nosniff",
-    ref: process.env.HEADERS_REFERRER_POLICY || "strict-origin-when-cross-origin"
+    access: {
+      headers: process.env.HEADERS_ACCESS_CONTROL_ALLOW_HEADERS || "Content-Type",
+      methods: process.env.HEADERS_ACCESS_CONTROL_ALLOW_METHODS || "OPTIONS, POST, GET, PUT, DELETE",
+      origin: process.env.HEADERS_ACCESS_CONTROL_ALLOW_ORIGIN || "*"
+    },
+    security: {
+      sts: process.env.HEADERS_STRICT_TRANSPORT_SECURITY || "max-age=31536000; includeSubDomains",
+      csp: process.env.HEADERS_CONTENT_SECURITY_POLICY || "default https:",
+      xfo: process.env.HEADERS_X_FRAME_OPTIONS || "SAMEORIGIN",
+      xss: process.env.HEADERS_X_XSS_PROTECTION || "1; mode=block",
+      xco: process.env.HEADERS_X_CONTENT_TYPE_OPTIONS || "nosniff",
+      ref: process.env.HEADERS_REFERRER_POLICY || "strict-origin-when-cross-origin"
+    }
   },
   lockers: {
     salt: process.env.LOCKERS_SALT,
