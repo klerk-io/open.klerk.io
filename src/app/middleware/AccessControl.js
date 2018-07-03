fe728e2aff899292652c074ca848fab85f1cbe05
Philipp Bauer <hello@philippbauer.org>
2018-07-03 16:30:02 -0500

    Added Access-Control-Expose-Headers for client integrations.

--- src/app/middleware/AccessControl.js
+++ src/app/middleware/AccessControl.js
@@ -3,6 +3,7 @@ function accesscontrol(config) {
     res.setHeader("Access-Control-Allow-Headers", config.headers);
     res.setHeader("Access-Control-Allow-Methods", config.methods);
     res.setHeader("Access-Control-Allow-Origin", config.origin);
+    res.setHeader("Access-Control-Expose-Headers", config.expose);
 
     next();
   }
