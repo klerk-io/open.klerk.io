8ff44a35581fc42336f51697a13158d260145f59
Philipp Bauer <hello@philippbauer.org>
2018-07-06 14:31:28 -0500

    Prevent OPTIONS method request to be logged as access.

--- src/app/middleware/AccessLog.js
+++ src/app/middleware/AccessLog.js
@@ -2,16 +2,18 @@ import RequireLibrary from "../libs/RequireLibrary";
 
 function accesslog(config) {
   return function(req, res, next) {
-    const Database = RequireLibrary('database', config.app.platform);
-    const database = new Database(config, config.logs.table.name);
+    if (req.method !== "OPTIONS") {
+      const Database = RequireLibrary('database', config.app.platform);
+      const database = new Database(config, config.logs.table.name);
 
-    const log = {
-      ip: req.ip,
-      path: req.path,
-      createdAt: Math.round(new Date().getTime() / 1000)
+      const log = {
+        ip: req.ip,
+        path: req.path,
+        createdAt: Math.round(new Date().getTime() / 1000)
+      }
+
+      database.store(log, null, null, config.logs.entity.indexes);
     }
-
-    database.store(log, null, null, config.logs.entity.indexes);
 
     next();
   };
