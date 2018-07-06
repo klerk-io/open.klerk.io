701f7c395f089ca08de56c77574672fec0f39874
Philipp Bauer <hello@philippbauer.org>
2018-07-06 14:30:00 -0500

    Reverted auto-formatting.

--- src/app/config.js
+++ src/app/config.js
@@ -8,25 +8,18 @@ module.exports = {
   },
   headers: {
     access: {
-      headers:
-        process.env.HEADERS_ACCESS_CONTROL_ALLOW_HEADERS || "Content-Type",
-      methods:
-        process.env.HEADERS_ACCESS_CONTROL_ALLOW_METHODS ||
-        "OPTIONS, POST, GET, PUT, DELETE",
+      headers: process.env.HEADERS_ACCESS_CONTROL_ALLOW_HEADERS || "Content-Type",
+      methods: process.env.HEADERS_ACCESS_CONTROL_ALLOW_METHODS || "OPTIONS, POST, GET, PUT, DELETE",
       origin: process.env.HEADERS_ACCESS_CONTROL_ALLOW_ORIGIN || "*",
-      expose:
-        process.env.HEADERS_ACCESS_CONTROL_EXPOSE_HEADERS || "Content-Length"
+      expose: process.env.HEADERS_ACCESS_CONTROL_EXPOSE_HEADERS || "Content-Length"
     },
     security: {
-      sts:
-        process.env.HEADERS_STRICT_TRANSPORT_SECURITY ||
-        "max-age=31536000; includeSubDomains",
+      sts: process.env.HEADERS_STRICT_TRANSPORT_SECURITY || "max-age=31536000; includeSubDomains",
       csp: process.env.HEADERS_CONTENT_SECURITY_POLICY || "default https:",
       xfo: process.env.HEADERS_X_FRAME_OPTIONS || "SAMEORIGIN",
       xss: process.env.HEADERS_X_XSS_PROTECTION || "1; mode=block",
       xco: process.env.HEADERS_X_CONTENT_TYPE_OPTIONS || "nosniff",
-      ref:
-        process.env.HEADERS_REFERRER_POLICY || "strict-origin-when-cross-origin"
+      ref: process.env.HEADERS_REFERRER_POLICY || "strict-origin-when-cross-origin"
     }
   },
   lockers: {
